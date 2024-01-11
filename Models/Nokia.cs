93b413bd7afd295ce59564b56424180ce4b4d851
Samuel Bezerra Bernardo <samuca_bernardo@outlook.com>
2024-01-11 18:14:54 -0300

    Criando um Sistema e Abstraindo um Celular com POO em C# - Desafio concluído!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
+    public class Nokia : Smartphone{
+        // I M P L E M E N T A D O ! ! !
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // I M P L E M E N T A D O ! ! !
+
+        public override void InstalarAplicativo(string nomeApp){
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Smartphone Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// I M P L E M E N T A D O ! ! !
+
+Console.WriteLine("----- Smartphone Nokia -----");
+Smartphone nokia = new Nokia(numero: "01234", modelo: "G60", imei: "1111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n--------------------------------------------------\n");
+
+Console.WriteLine("----- Smartphone iPhone -----");
+Smartphone iphone = new Iphone(numero: "56789", modelo: "iPhone 15 Pro Max", imei: "2222222222", memoria: 1024);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n--------------------------------------------------\n");
